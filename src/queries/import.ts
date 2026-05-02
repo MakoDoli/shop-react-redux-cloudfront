6b94cffb17e3769c2b04b55d5dc811b449676083
MakoDoli <makodolidze@hotmail.com>
2026-05-02 23:33:17 +0400

    integrate new lambda endpoint

---
+++ src/queries/import.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import API_PATHS from "~/constants/apiPaths";
+
+type GetSignedUrlResponse = {
+  signedUrl: string;
+};
+
+export async function getSignedUrl(
+  fileName: string,
+  importEndpoint = `${API_PATHS.import}/import`,
+): Promise<string> {
+  const encodedFileName = encodeURIComponent(fileName);
+  const res = await axios.get<GetSignedUrlResponse>(
+    `${importEndpoint}?name=${encodedFileName}`,
+  );
+  return res.data.signedUrl;
+}
+
+export async function uploadCsvToS3(signedUrl: string, file: File) {
+  return axios.put(signedUrl, file, {
+    headers: {
+      "Content-Type": "text/csv",
+    },
+  });
+}
+
+export async function uploadImportFile(file: File) {
+  const signedUrl = await getSignedUrl(file.name);
+  await uploadCsvToS3(signedUrl, file);
+}
